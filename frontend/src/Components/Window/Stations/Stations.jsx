bdcae5593cd1aaa7421081d9b71c518c8bcc36be
Gagandeep Singh <gagan.official2k1@gmail.com>
2023-12-16 15:26:36 +0530

    Modified Stations data

--- frontend/src/Components/Window/Stations/Stations.jsx
+++ frontend/src/Components/Window/Stations/Stations.jsx
@@ -1,14 +1,55 @@
 import styles from "./Stations.module.css";
 
 function Stations() {
-  const staticData = [1, 2, 3, 4, 5, 6];
+  const colors = {
+    pink: "#e692be",
+    yellow: "#f5d618",
+    red: "#c1282b",
+  };
+  const staticDataa = [
+    {
+      id: 1,
+      name: "Dilli Haat - INA",
+      color: colors.yellow,
+    },
+    {
+      id: 2,
+      name: "Kashmiri Gate",
+      color: colors.yellow,
+    },
+    {
+      id: 3,
+      name: "Bhikaji Cama Place",
+      color: colors.pink,
+    },
+    {
+      id: 4,
+      name: "Lajpath Nagar",
+      color: colors.pink,
+    },
+    {
+      id: 5,
+      name: "Rithala",
+      color: colors.red,
+    },
+    {
+      id: 6,
+      name: "Sarojini Nagar",
+      color: colors.pink,
+    },
+    {
+      id: 7,
+      name: "Rajouri Garden",
+      color: colors.pink,
+    },
+  ];
   return (
     <seciton className={styles.stationsCont}>
-      {staticData.map((items, index) => {
+      {staticDataa.map((items, index) => {
         return (
           <div key={index} className={styles.stationTile}>
-            <span style={{ backgroundColor: "#FFD600" }}>{items}</span> Dilli
-            Haat - INA
+            <span style={{ backgroundColor: items.color }}>{items.id}</span>
+            {items.name}
           </div>
         );
       })}
